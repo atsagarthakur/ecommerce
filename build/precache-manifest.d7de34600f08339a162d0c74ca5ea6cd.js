8021d796b79c669ad35136ef1496a438a7abd95b
Sagar Thakur <139774619+atsagarthakur@users.noreply.github.com>
2024-04-05 03:40:24 +0530

    Add files via upload

---
+++ build/precache-manifest.d7de34600f08339a162d0c74ca5ea6cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e1e4670fbb1c1b29288b0a54240398",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eca001f971d3c268b00b",
+    "url": "/static/css/main.d3f13e23.chunk.css"
+  },
+  {
+    "revision": "61a46c2cc602a89981a7",
+    "url": "/static/js/2.ceef16a7.chunk.js"
+  },
+  {
+    "revision": "038db422c0414d8dfd31541a1bf2a15e",
+    "url": "/static/js/2.ceef16a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca001f971d3c268b00b",
+    "url": "/static/js/main.966335e4.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
